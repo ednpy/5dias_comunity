c94aa3380eefb0243b5ef0735e92edc50c08aac6
Mattias Peralta <mattiasperalta1995@gmail.com>
2025-02-10 08:51:24 -0300

    first commit

---
+++ frontend/src/pages/auth/SignUpPage.jsx
@@ -0,0 +1,40 @@
+import { Link } from "react-router-dom";
+import SignUpForm from "../../components/auth/SignUpForm";
+
+const SignUpPage = () => {
+	return (
+		<div className='min-h-screen flex flex-col justify-center py-12 sm:px-6 lg:px-8'>
+			<div className='sm:mx-auto sm:w-full sm:max-w-md'>
+				<img className='mx-auto h-36 w-auto' src='/logo.svg' alt='LinkedIn' />
+				<h2 className='text-center text-3xl font-extrabold text-gray-900'>
+					Potencia al máximo tu vida profesional
+				</h2>
+			</div>
+			<div className='mt-8 sm:mx-auto sm:w-full sm:max-w-md shadow-md'>
+				<div className='bg-secondary py-8 px-4 shadow sm:rounded-lg sm:px-10'>
+					<SignUpForm />
+
+					<div className='mt-6'>
+						<div className='relative'>
+							<div className='absolute inset-0 flex items-center'>
+								<div className='w-full border-t border-gray-300'></div>
+							</div>
+							<div className='relative flex justify-center text-sm'>
+								<span className='px-2 bg-secondary text-gray-500'>¿Ya estás en la comunidad?</span>
+							</div>
+						</div>
+						<div className='mt-6'>
+							<Link
+								to='/login'
+								className='text-primary w-full flex justify-center py-2 px-4 border border-transparent rounded-md shadow-sm text-sm font-medium text-blue-600 bg-white hover:bg-gray-50'
+							>
+								Logueate
+							</Link>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	);
+};
+export default SignUpPage;
